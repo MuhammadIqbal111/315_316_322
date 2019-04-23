1c5dce9997bf9a6a42f5152deef52f73071f900b
MuhammadIqbal111 <38502098+MuhammadIqbal111@users.noreply.github.com>
2019-04-23 15:24:09 +0500

    Food Corner Project Design

---
+++ FoodCorner/FoodCorner/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FoodCorner.MvcApplication" Language="C#" %>
---
+++ FoodCorner/FoodCorner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodCorner.Startup))]
+namespace FoodCorner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
